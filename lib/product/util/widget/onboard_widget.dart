15508dca33a361b70ce18b1e76909d2c4ce08bcb
nuriulgen <bazid04ulgen@gmail.com>
2022-07-24 20:03:53 +0300

    Create onboard widget componet.

---
+++ lib/product/util/widget/onboard_widget.dart
@@ -0,0 +1,67 @@
+import 'package:e_commerce_app/core/utility/extension/color/color_extension.dart';
+import 'package:e_commerce_app/core/utility/extension/context/context_extension.dart';
+import 'package:flutter/material.dart';
+
+import '../../../core/constant/image/image_constants.dart';
+
+class OnBoardWidget extends StatelessWidget {
+  const OnBoardWidget({
+    Key? key,
+    required this.title,
+    required this.subTitle,
+    required this.imagePath,
+  }) : super(key: key);
+
+  final String title;
+  final String subTitle;
+  final String imagePath;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: context.padding2xBottom,
+          child: _placeImage,
+        ),
+        SizedBox(height: context.highValue),
+        Padding(
+          padding: context.paddingXVertical,
+          child: _title(context),
+        ),
+        Padding(
+          padding: context.paddingXVertical,
+          child: _subTitle(context),
+        ),
+
+        // Expanded(child: _header),
+      ],
+    );
+  }
+
+  SizedBox _subTitle(BuildContext context) {
+    return SizedBox(
+      height: context.hw45,
+      width: context.hw330,
+      child: Text(
+        subTitle,
+        style: Theme.of(context)
+            .textTheme
+            .subtitle2
+            ?.copyWith(color: context.heatherGrey, fontWeight: FontWeight.w400),
+      ),
+    );
+  }
+
+  Text _title(BuildContext context) {
+    return Text(
+      title,
+      style: Theme.of(context)
+          .textTheme
+          .headline6
+          ?.copyWith(color: context.chasm, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Widget get _placeImage => PngImage(imagePath: imagePath);
+}
