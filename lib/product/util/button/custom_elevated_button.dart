348d8e9e338df76d39b8b7bafcda8544b1c813af
nuriulgen <bazid04ulgen@gmail.com>
2022-07-24 03:34:32 +0300

    Create Custom Elevated Button.

---
+++ lib/product/util/button/custom_elevated_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  CustomElevatedButton(
+      {Key? key,
+      required this.title,
+      required this.onPressed,
+      required this.color,
+      required this.textColor})
+      : super(key: key);
+
+  final String title;
+  final Color color;
+  final Color textColor;
+  final void Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          primary: color,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+      onPressed: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .subtitle2
+              ?.copyWith(color: textColor, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
